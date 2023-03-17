521a18e6b1e19aa6eb60eddfc9c4b47564fd39b2
marathnamalala <2012nuwan@gmail.com>
2023-03-17 12:38:31 +0530

    Exercise 4

---
+++ Exercise4/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * @author marathnamalala
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the Sentence");
+
+    //Removed white spaces as the result doesn't contain the white space
+    String inputString = scanner.nextLine().replaceAll("\\s+", "");
+
+    Map<Character, Integer> lettersCountMap = new LinkedHashMap<>();
+
+    for (char letter : inputString.toCharArray()) {
+
+      if (lettersCountMap.containsKey(letter)) {
+        System.out.println("sss");
+        lettersCountMap.put(letter, lettersCountMap.get(letter) + 1);
+
+      } else {
+        lettersCountMap.put(letter, 1);
+      }
+    }
+
+    for (Map.Entry<Character, Integer> entry : lettersCountMap.entrySet()) {
+      System.out.println("● : " + entry.getKey() + " " + entry.getValue());
+    }
+  }
+}
